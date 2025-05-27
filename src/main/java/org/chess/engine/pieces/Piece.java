54d3f9cc5f8e50c924e5954f1931d5dd938de131
mikolaj-jalocha <mikics2k17@gmail.com>
2025-05-27 14:06:30 +0200

    feat: add Pawn class

--- src/main/java/org/chess/engine/pieces/Piece.java
+++ src/main/java/org/chess/engine/pieces/Piece.java
@@ -9,16 +9,21 @@ import java.util.Collection;
 public abstract class Piece {
 
     protected final int piecePosition;
+    protected final Alliance pieceAlliance;
+    protected final boolean isFirstMove;
+
+    Piece(final int piecePosition, final Alliance pieceAlliance){
+        this.piecePosition = piecePosition;
+        this.pieceAlliance = pieceAlliance;
+        this.isFirstMove = false;
+    }
 
     public Alliance getPieceAlliance() {
         return pieceAlliance;
     }
 
-    protected final Alliance pieceAlliance;
-
-    Piece(final int piecePosition, final Alliance pieceAlliance){
-        this.piecePosition = piecePosition;
-        this.pieceAlliance = pieceAlliance;
+    public boolean isFirstMove() {
+        return isFirstMove;
     }
 
     public abstract Collection<Move> calculateLegalMoves(final Board board);
