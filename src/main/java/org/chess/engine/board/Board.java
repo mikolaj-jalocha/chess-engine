048bbf78c23d8949fdff25c5a9fc7e2bf1d311dc
mikolaj-jalocha <mikics2k17@gmail.com>
2025-05-31 20:11:15 +0200

    feat: introduce builder pattern to Board class

--- src/main/java/org/chess/engine/board/Board.java
+++ src/main/java/org/chess/engine/board/Board.java
@@ -1,9 +1,56 @@
 package org.chess.engine.board;
 
+import com.google.common.collect.ImmutableList;
+import org.chess.engine.Alliance;
+import org.chess.engine.pieces.Piece;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
 public class Board {
+
+    private final List<Tile> gameBoard;
+
+    private Board(Builder builder){
+        this.gameBoard = createGameBoard(builder);
+    }
 
     public Tile getTile(final int tileCoordinate){
         return null;
+    }
+
+    private static List<Tile> createGameBoard(final Builder builder){
+        final Tile[] tiles = new Tile[BoardUtils.NUM_TILES];
+        for (int i =  0; i < BoardUtils.NUM_TILES; i++){
+            tiles[i] = Tile.createTile(i,builder.boardConfig.get(i));
+        }
+        return ImmutableList.copyOf(tiles);
+    }
+
+    public static class Builder {
+
+        Map<Integer, Piece> boardConfig;
+        Alliance nextMoveMaker;
+
+        public Builder() {
+
+        }
+
+        public Builder setPiece(final Piece piece){
+            this.boardConfig.put(piece.getPiecePosition(), piece);
+            return this;
+        }
+
+        public Builder setMoveMaker(final Alliance nextMoveMaker){
+            this.nextMoveMaker = nextMoveMaker;
+            return this;
+        }
+
+        public Board build(){
+            return new Board(this);
+        }
+
     }
 
 }
